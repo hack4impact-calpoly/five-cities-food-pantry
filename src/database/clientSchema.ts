f719c801bf8ecce0472e94c8fb7583870615e65d
Aidan Nesbitt <aidannesbitt20@gmail.com>
2024-02-18 16:51:36 -0800

    Revert "feat: created API endpoint for all clients /clients and for a single client, /clients/userId"
    This reverts commit 8c31d767abec206e58a8b0a44f9b6d1de74359ac.

--- src/database/clientSchema.ts
+++ src/database/clientSchema.ts
@@ -11,56 +11,45 @@ interface iClient extends Document {
 
 const ClientSchema = new Schema<iClient>({
   name: {
-    type: String,
-    required: true,
+    type: String, 
+    required: true
   },
   age: {
-    type: Number,
-    required: true,
-    min: 0,
-    max: 120,
+    type: Number, 
+    required: true, 
+    min: 0, 
+    max: 120
   },
-  entryDates: [
-    {
-      type: Date,
-      default: Date.now,
-    },
-  ],
+  entryDates: [{
+    type: Date, 
+    default: Date.now
+  }],
   phoneNumber: {
     type: String,
     required: true,
     // Make sure phone number only contains numerical characters
     validate: {
-      validator: function (v: string) {
-        // Check if the phone number contains only numerical characters
-        return /^\d+$/.test(v);
+      validator: function(v: string) {
+          // Check if the phone number contains only numerical characters
+          return /^\d+$/.test(v);
       },
-      message: (props: { value: string }) =>
-        `${props.value} is not a valid phone number.`,
-    },
+      message: (props: {value: string}) => `${props.value} is not a valid phone number.`
+    }
   },
   email: {
-    type: String,
+    type: String, 
     required: true,
-    unique: true,
+    unique: true, 
     lowercase: true,
     // validate email using regex -- better way of validation?
-    match: [
-      /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
-      "Please fill a valid email address",
-    ],
+    match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']
   },
   address: {
-    type: String,
-    required: true,
+    type: String, 
+    required: true
     // validation ?
-  },
+  }
   // family_unit: TBA
 });
 
-//if you omit the third argument here, mongoose will access a lowercase, pluralized version of the model name "client"
-//with this set up, it will access the 'client' collection within the ccwfp database
-const IClientSchema =
-  mongoose.models["client"] || mongoose.model("client", ClientSchema, "client");
-
-export { IClientSchema };
+export default mongoose.models.Client || mongoose.model<iClient>('Client', ClientSchema);
