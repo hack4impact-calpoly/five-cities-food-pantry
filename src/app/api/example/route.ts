4bb3089a50c6676d6120c22e57daa6f8ae2287a9
lamaro02 <amarolindsey@gmail.com>
2024-02-19 21:47:08 -0800

    Remove some code lines that were for testing

--- src/app/api/example/route.ts
+++ src/app/api/example/route.ts
@@ -1,6 +1,5 @@
 import connectDB from "@database/db";
 import { NextResponse } from "next/server";
-import Employee from "../../../database/employeeSchema";
 
 /**
  * Example GET API route
@@ -8,6 +7,5 @@ import Employee from "../../../database/employeeSchema";
  */
 export async function GET() {
   await connectDB();
-  const employees = await Employee.find();
   return NextResponse.json({ message: "Hello from the API!" });
 }
