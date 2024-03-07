5ceef647b6f3115bce8a5f23ddd603eb2ea72111
Aidan Nesbitt <aidannesbitt20@gmail.com>
2024-03-07 10:34:38 -0800

    feat: created ClientDashboard component, and created the clientPage view on the site

---
+++ src/app/clientPage/page.tsx
@@ -0,0 +1,11 @@
+import Navbar from "../components/Navbar";
+import ClientDashboard from "../components/clientDashboard";
+
+export default function ClientPage() {
+  return (
+    <>
+      <Navbar />
+      <ClientDashboard />
+    </>
+  );
+}
