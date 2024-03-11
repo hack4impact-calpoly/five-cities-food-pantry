348382e282acf495b77ad53bde778e1167f477e8
Arya Ramchander <aryaramchander7238@gmail.com>
2024-03-10 20:29:21 -0700

    add new client page frontend

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,3 +1,34 @@
-export default function Navbar() {
-  return <div>Navbar</div>;
-}
+/* eslint-disable @next/next/no-img-element */
+import React from "react";
+import "./Navbar.css";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <div className="navbar-container">
+        <a href="/" className="navbar-link">
+          <img
+            src="/logo.png"
+            alt="Five Cities Christian Women Food Pantry Logo"
+            className="logo"
+          />
+        </a>
+        <div className="navbar-right-links">
+          <a href="/clientSearch" className="navbar-link">
+            Client Search
+          </a>
+          <a href="/report" className="navbar-link">
+            Generate PDF
+          </a>
+          <div className="profile-icon">
+            <svg className="icon">
+              <use href="/user-icons.svg#icon-user-profile" />
+            </svg>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
