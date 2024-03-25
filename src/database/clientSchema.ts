54ceaccdaf8e151165ab7444eb7ee2daf25bda68
Aidan Nesbitt <aidannesbitt20@gmail.com>
2024-03-24 17:04:06 -0700

    feat: fixed data validation for birthDate to calculate the date and still check if 0<birthDate<120

--- src/database/clientSchema.ts
+++ src/database/clientSchema.ts
@@ -13,6 +13,22 @@ interface iClient extends Document {
   isFlagged: boolean;
 }
 
+// used in validation for the date attribute of the client schema
+const calculateAge = (birthDate: Date) => {
+  const today = new Date();
+  const birthDateObj = new Date(birthDate);
+
+  let age = today.getFullYear() - birthDateObj.getFullYear();
+  const m = today.getMonth() - birthDateObj.getMonth();
+
+  //decrease age by 1 if it has not yet occurred this year
+  if (m < 0 || (m === 0 && today.getDate() < birthDateObj.getDate())) {
+    age--;
+  }
+
+  return age;
+};
+
 const ClientSchema = new Schema<iClient>({
   firstName: {
     type: String,
@@ -25,8 +41,13 @@ const ClientSchema = new Schema<iClient>({
   birthDate: {
     type: Date,
     required: true,
-    min: 0,
-    max: 120,
+    validate: {
+      validator: function (v: Date) {
+        const age = calculateAge(v);
+        return age >= 0 && age <= 120;
+      },
+      message: "Client age must be between 0 and 120 years.",
+    },
   },
   entryDates: [
     {
