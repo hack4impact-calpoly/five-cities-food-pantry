b51f845665efd009814a302d142edc7941f20cc9
Aidan Nesbitt <aidannesbitt20@gmail.com>
2024-04-17 20:03:47 -0700

    feat: sign up page mostly functional, now will create confirmation page

---
+++ src/app/api/signup/route.ts
@@ -0,0 +1,78 @@
+import { NextRequest, NextResponse } from "next/server";
+import connectDB from "../../../database/db";
+import IUserSchema from "../../../database/userSchema";
+
+// expected request body
+interface SignupRequestBody {
+  email: string;
+  password: string;
+  firstname: string;
+  lastname: string;
+}
+
+// A utility function to parse the request body
+async function parseBody(request: NextRequest) {
+  const contentType = request.headers.get("content-type");
+  if (contentType?.includes("application/json")) {
+    return request.json();
+  }
+  return {};
+}
+
+export async function POST(req: NextRequest) {
+  try {
+    // Establishing database connection
+    await connectDB();
+
+    // error if not typecasted to unknown first
+    const { email, password, firstname, lastname } = (await parseBody(
+      req
+    )) as SignupRequestBody;
+
+    console.log(
+      "inputted: email ",
+      email,
+      " password: ",
+      password,
+      " firstname: ",
+      firstname,
+      " lastname: ",
+      lastname
+    );
+
+    // Attempting to find user associated with inputted email
+    const user = await IUserSchema.findOne({ email: email });
+
+    // * user does not exist, therefore creates a document with the user's information
+    // ! NOTE: PASSWORD HASHING MUST OCCUR HERE.
+    if (!user) {
+      // User does not exist, create a document with the user's information
+      const newUser = await IUserSchema.create({
+        email: email,
+        password: password,
+        firstname: firstname,
+        lastname: lastname,
+      });
+
+      console.log("NEW USER: ", newUser);
+
+      return NextResponse.json(
+        { message: "User successfully created." },
+        { status: 201 }
+      );
+    } else if (user) {
+      // ! what status code to use here?
+      return NextResponse.json(
+        { message: "User with inputted email found, please login." },
+        { status: 409 }
+      );
+    }
+  } catch (err) {
+    console.error("Error fetching user:", err);
+    // ! how to make this error more descriptive?
+    return NextResponse.json(
+      { error: "Error occured with finding user." },
+      { status: 500 }
+    );
+  }
+}
