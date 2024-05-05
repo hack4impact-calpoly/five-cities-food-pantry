fd15d4f19675e5ba3f063a2062dbe991cc52c1b6
lamaro02 <amarolindsey@gmail.com>
2024-05-05 13:09:14 -0700

    profile page ui

---
+++ src/app/profilePage/page.tsx
@@ -0,0 +1,29 @@
+import style from "./profilePage.module.css"
+import Navbar from "@components/Navbar"
+
+export default function ProfilePage() {
+    return <div>
+        <Navbar></Navbar>
+        <div className={style.profilePage}>
+        <div className={style.icons}>
+        <svg><use href="/user-icons.svg#icon-user-profile" /></svg>
+        <svg width="100x" height="50px"><use href="/user-icons.svg#icon-pencil"/></svg>
+        </div>
+        <div className={style.userInfo}>
+        <div className={style.nameInfo}>
+            <div className={style.nameItem}>
+            <p><strong>First Name</strong></p>
+            <p className={style.infoBox}>First name here</p>
+            </div>
+            <div className={style.nameItem}>
+            <p><strong>Last Name</strong></p>
+            <p className={style.infoBox}>Last Name here</p>
+            </div>
+        </div>
+        <p><strong>Email</strong></p>
+        <p className={style.infoBox}>Email here</p>
+        <button className={style.logout}><strong>Log out</strong></button>
+        </div>
+        </div>
+    </div>
+}
