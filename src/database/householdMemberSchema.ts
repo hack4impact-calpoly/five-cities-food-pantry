4ca4f58af15e86d72e9e8cca5c47fd44174524b3
Aidan Nesbitt <aidannesbitt20@gmail.com>
2024-05-13 12:12:36 -0700

    fix: added an optional current field to householdMemberSchema

--- src/database/householdMemberSchema.ts
+++ src/database/householdMemberSchema.ts
@@ -1,36 +1,45 @@
 import mongoose, { Schema, Types } from "mongoose";
 
 interface IHouseholdMember extends Document {
-    firstName: string;
-    lastName: string;
-    birthDate: Date;
-    isAdult: boolean;
-    headHousehold: Types.ObjectId;
+  firstName: string;
+  lastName: string;
+  birthDate: Date;
+  isAdult: boolean;
+  headHousehold: Types.ObjectId;
+  current: boolean;
 }
 
 const HouseholdMemberSchema = new Schema<IHouseholdMember>({
-    firstName: {
-        type: String,
-        required: true
-    },
-    lastName: {
-        type: String,
-        required: true
-    },
-    birthDate: {
-        type: Date,
-        required: true
-    },
-    isAdult: {
-        type: Boolean,
-        required: true
-    },
-    headHousehold: {
-        type: Schema.Types.ObjectId,
-        ref: 'Client',
-        required: true 
-    }
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  birthDate: {
+    type: Date,
+    required: true,
+  },
+  isAdult: {
+    type: Boolean,
+    required: true,
+  },
+  current: {
+    type: Boolean,
+    required: false,
+  },
+  headHousehold: {
+    type: Schema.Types.ObjectId,
+    ref: "Client",
+    required: true,
+  },
 });
 
 export default mongoose.models.HouseholdMember ||
-  mongoose.model<IHouseholdMember>("HouseholdMember", HouseholdMemberSchema, "householdMember");
+  mongoose.model<IHouseholdMember>(
+    "HouseholdMember",
+    HouseholdMemberSchema,
+    "householdMember"
+  );
